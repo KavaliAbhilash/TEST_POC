3e4509bb18f55be8b035482013c569d2b7bbc998
KavaliAbhilash <32427197+KavaliAbhilash@users.noreply.github.com>
2022-11-10 11:26:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "harness"
+  location = "centralindia"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "harnessdevopsRegistry1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
